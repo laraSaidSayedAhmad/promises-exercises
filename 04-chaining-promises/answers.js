c02279f97e154953269829d203cf6b5c071992d1
lara <azizasayedahmad@gmail.com>
2021-12-02 20:26:27 +0200

    chaining-promises

--- 04-chaining-promises/answers.js
+++ 04-chaining-promises/answers.js
@@ -1,38 +1,60 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {Promise} promise 
- * @param {function} asyncTransformer 
+ *
+ * @param {Promise} promise
+ * @param {function} asyncTransformer
  */
-function flatMapPromise(promise, asyncTransformer){
+function flatMapPromise(promise, asyncTransformer) {
   return new Promise((resolve, reject) => {
     promise
-      .then(/* IMPLEMENT ME! */);
+      .then((res) => {
+        resolve(asyncTransformer(res));
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} firstPromise 
- * @param {function} slowAsyncProcess 
+ *
+ * @param {Promise} firstPromise
+ * @param {function} slowAsyncProcess
  */
-function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
-  return firstPromise.then(/* IMPLEMENT ME! */);
+
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess) {
+  return firstPromise.then((res) => {
+    return Promise.resolve(slowAsyncProcess(res));
+  });
+}
+/**
+ *
+ * EXERCISE 3
+ *
+ * @param {function} getUserById
+ * @param {function} getOrganizationById
+ */
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById) {
+  return function getUserByIdWithOrganization(userId) {
+    /* IMPLEMENT ME! */
+    return getUserById((userId) =>
+      getOrganizationById(user.OrganizationId).then(
+        (orgObj) => (user.organization = orgObj)
+      )
+    );
+  };
 }
 
-/**
- * 
- * EXERCISE 3
- * 
- * @param {function} getUserById 
- * @param {function} getOrganizationById 
- */
-function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
-  return function getUserByIdWithOrganization(userId){
-    /* IMPLEMENT ME! */
+function combinedObject(userId) {
+  return function getUserByIdWithOrganization(userId) {
+    const user = grtUserById(userId);
+    let combinedUserObject = getOrganizationById(user.OrganizationId).then(
+      (orgObj) => (user.organization = orgObj)
+    );
+    return combinedUserObject;
   };
 }
 
